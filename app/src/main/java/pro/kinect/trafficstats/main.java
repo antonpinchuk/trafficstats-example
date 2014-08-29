251c0c9c0d5f80029d748913b7dc5c0aa0204d94
Maxim Kolesnikov <mpkmaximus@gmail.com>
2014-08-29 13:32:02 +0300

    - add application traffic list

--- app/src/main/java/pro/kinect/trafficstats/main.java
+++ app/src/main/java/pro/kinect/trafficstats/main.java
@@ -1,18 +1,54 @@
 package pro.kinect.trafficstats;
 
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.os.Handler;
 import android.net.TrafficStats;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 
 
 public class main extends Activity {
 
     private TextView tvSupported, tvDataUsageWiFi, tvDataUsageMobile, tvDataUsageTotal;
+    private ListView lvApplications;
+
+    private long totalCurrent = 0;
+
+    private class Application {
+        long tx = 0;
+        long rx = 0;
+        long total = 0;
+        int uid;
+        String Name;
+
+        Application(int _uid ,String _name){
+            uid = _uid; Name = _name;
+        }
+
+        public void update(){
+            tx = TrafficStats.getUidTxBytes(uid);
+            rx = TrafficStats.getUidRxBytes(uid);
+            total = tx + rx;
+        }
+    }
+
+    private ArrayList<Application> Applications =  new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,35 +60,63 @@ public class main extends Activity {
         tvDataUsageMobile = (TextView) findViewById(R.id.tvDataUsageMobile);
         tvDataUsageTotal = (TextView) findViewById(R.id.tvDataUsageTotal);
 
+        lvApplications = (ListView) findViewById(R.id.lvInstallApplication);
+
         if (TrafficStats.getTotalTxBytes() != TrafficStats.UNSUPPORTED) {
             handler.postDelayed(runnable, 0);
         } else {
             tvSupported.setText("Not supported");
             tvSupported.setTextColor(ColorStateList.valueOf(0xFFFF0000));
         }
-    }
 
+        final ArrayAdapter<Application> adapterApplications = new ArrayAdapter<Application>(getApplicationContext(), R.layout.item_install_application) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+                final View result;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+                if (convertView == null) {
+                    result = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_install_application, parent, false);
+                } else {
+                    result = convertView;
+                }
+
+                TextView tvAppName = (TextView) result.findViewById(R.id.tvAppName);
+                TextView tvAppTraffic = (TextView) result.findViewById(R.id.tvAppTraffic);
+
+                Application app = (Application) getItem(position);
+
+                tvAppName.setText(app.Name);
+                tvAppTraffic.setText(Long.toString(app.total / 1024) + " Kb");
+
+                return result;
+            }
+            @Override
+            public int getCount() {
+                return super.getCount();
+            }
+        };
+
+        for (ApplicationInfo app : getApplicationContext().getPackageManager().getInstalledApplications(0)) {
+            PackageManager packageManager = getApplicationContext().getPackageManager();
+            String name = packageManager.getApplicationLabel(app).toString();
+            Application application = new Application(app.uid, name);
+            application.update();
+            if( (application.total / 1024) > 0) adapterApplications.add(application);
         }
-        return super.onOptionsItemSelected(item);
+
+        adapterApplications.sort(new Comparator<Application>() {
+            @Override
+            public int compare(Application lhs, Application rhs) {
+                return (int)(rhs.total - lhs.total);
+            }
+        });
+
+        lvApplications.setAdapter(adapterApplications);
     }
 
     public Handler handler = new Handler();
+
     public Runnable runnable = new Runnable() {
         public void run() {
             long mobile = TrafficStats.getMobileRxBytes() + TrafficStats.getMobileTxBytes();
@@ -60,6 +124,13 @@ public class main extends Activity {
             tvDataUsageWiFi.setText("" + (total - mobile) / 1024 + " Kb");
             tvDataUsageMobile.setText("" + mobile / 1024 + " Kb");
             tvDataUsageTotal.setText("" + total / 1024 + " Kb");
+
+            if(totalCurrent != total) {
+                totalCurrent = total;
+
+
+            }
+
             handler.postDelayed(runnable, 3000);
         }
     };
