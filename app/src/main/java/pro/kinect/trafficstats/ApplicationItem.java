ec841bda6fd26bf98d4752611931f675879bcba1
Anton Pinchuk <antonpinchuk@yandex.ru>
2014-09-03 12:45:41 +0300

    refactoring (in progress)

--- app/src/main/java/pro/kinect/trafficstats/ApplicationItem.java
+++ app/src/main/java/pro/kinect/trafficstats/ApplicationItem.java
@@ -1,4 +1,4 @@
-package pro.kinect.trafficstats;
+package com.truumobile.datausage;
 
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
@@ -6,67 +6,81 @@ import android.graphics.drawable.Drawable;
 import android.net.TrafficStats;
 
 public class ApplicationItem {
+    /**
+     * Absolute since device boot
+     */
     private long tx = 0;
     private long rx = 0;
 
-    private long wifi_tx = 0;
-    private long wifi_rx = 0;
+    /**
+     * Absolute since device boot
+     */
+    private long txLast = 0;
+    private long rxLast = 0;
 
-    private long mobil_tx = 0;
-    private long mobil_rx = 0;
+    /**
+     * Resettable counters
+     */
+    private long txWifi = 0;
+    private long rxWifi = 0;
+    private long txMobile = 0;
+    private long rxMobile = 0;
 
-    private long current_tx = 0;
-    private long current_rx = 0;
-
-    private ApplicationInfo app;
-
-    private boolean isMobil = false;
+    private ApplicationInfo applicationInfo;
 
     public ApplicationItem(ApplicationInfo _app) {
-        app = _app;
-        update();
+        applicationInfo = _app;
+
+        // First time get current counter
+        rxLast = TrafficStats.getUidRxBytes(applicationInfo.uid);
+        txLast = TrafficStats.getUidTxBytes(applicationInfo.uid);
     }
 
-    public void update() {
-        long delta_tx = TrafficStats.getUidTxBytes(app.uid) - tx;
-        long delta_rx = TrafficStats.getUidRxBytes(app.uid) - rx;
+    public void update(boolean isMobile) {
+        long rx = TrafficStats.getUidRxBytes(applicationInfo.uid);
+        long tx = TrafficStats.getUidTxBytes(applicationInfo.uid);
 
-        tx = TrafficStats.getUidTxBytes(app.uid);
-        rx = TrafficStats.getUidRxBytes(app.uid);
+        long rxDelta = rx - rxLast;
+        long txDelta = tx - txLast;
 
-        current_tx = current_tx + delta_tx;
-        current_rx = current_rx + delta_rx;
-
-        if(isMobil == true) {
-            mobil_tx = mobil_tx + delta_tx;
-            mobil_rx = mobil_rx + delta_rx;
+        if (isMobile) {
+            rxMobile = rxMobile + rxDelta;
+            txMobile = txMobile + txDelta;
         } else {
-            wifi_tx = wifi_tx + delta_tx;
-            wifi_rx = wifi_rx + delta_rx;
+            rxWifi = rxWifi + rxDelta;
+            txWifi = txWifi + txDelta;
         }
+
+        rxLast = rx;
+        txLast = tx;
     }
 
-    public static ApplicationItem create(ApplicationInfo _app){
-        long _tx = TrafficStats.getUidTxBytes(_app.uid);
-        long _rx = TrafficStats.getUidRxBytes(_app.uid);
 
-        if((_tx + _rx) > 0) return new ApplicationItem(_app);
-        return null;
-    }
+
+//    public static ApplicationItem create(ApplicationInfo _applicationInfo) {
+//        long _tx = TrafficStats.getUidTxBytes(_applicationInfo.uid);
+//        long _rx = TrafficStats.getUidRxBytes(_applicationInfo.uid);
+//
+//        if ((_tx + _rx) > 0) return new ApplicationItem(_applicationInfo);
+//        return null;
+//    }
 
     public int getTotalUsageKb() {
-        return Math.round((tx + rx)/ 1024);
+        return Math.round((tx + rx) / 1024);
+    }
+    public int getMobileUsageKb() {
+        return Math.round((txMobile + rxMobile) / 1024);
+    }
+    public int getWifiUsageKb() {
+        return Math.round((txWifi + rxWifi) / 1024);
     }
 
-    public String getApplicationLabel(PackageManager _packageManager) {
-        return _packageManager.getApplicationLabel(app).toString();
+    public String getApplicationLabel(PackageManager packageManager) {
+        return packageManager.getApplicationLabel(applicationInfo).toString();
     }
 
-    public Drawable getIcon(PackageManager _packageManager) {
-        return _packageManager.getApplicationIcon(app);
+    public Drawable getIcon(PackageManager packageManager) {
+        return packageManager.getApplicationIcon(applicationInfo);
     }
 
-    public void setMobilTraffic(boolean _isMobil) {
-        isMobil = _isMobil;
-    }
 }
--- app/src/main/java/pro/kinect/trafficstats/ApplicationList.java
+++ app/src/main/java/pro/kinect/trafficstats/ApplicationList.java
@@ -1,9 +1,9 @@
-package pro.kinect.trafficstats;
+package com.truumobile.datausage;
 
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+
+import com.common.Networks;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,16 +19,16 @@ public class ApplicationList {
     private int TIME_APPLICATION_UPDATE = 3 * 1000; // 3 second
 
     private boolean isWifiEnabled = false;
-    private boolean isMobilEnabled = false;
+    private boolean isMobile = false;
 
     private List<ApplicationItem> mApplicationItemList = new ArrayList<ApplicationItem>();
 
-    public ApplicationList(Context _context) {
-        mContext = _context;
+    public ApplicationList(Context context) {
+        mContext = context;
         updateNetworkState();
     }
 
-    public void Start() {
+    public void start() {
         mTask = new TimerTask() {
             @Override
             public void run() {
@@ -40,7 +40,7 @@ public class ApplicationList {
         mTimer.schedule(mTask, 0, TIME_APPLICATION_UPDATE);
     }
 
-    public void Stop() {
+    public void stop() {
         if (mTimer != null) {
             mTimer.purge();
             mTimer.cancel();
@@ -53,40 +53,24 @@ public class ApplicationList {
 
     public void update() {
         updateNetworkState();
-        if(mApplicationItemList != null) {
+        if (mApplicationItemList != null) {
             for (int i = 0, l = mApplicationItemList.size(); i < l; i++) {
-                mApplicationItemList.get(i).setMobilTraffic(isMobilEnabled);
-                mApplicationItemList.get(i).update();
+                mApplicationItemList.get(i).update(isMobile);
             }
         } else {
             for (ApplicationInfo app : mContext.getPackageManager().getInstalledApplications(0)) {
                 ApplicationItem item = new ApplicationItem(app);
-                item.setMobilTraffic(isMobilEnabled);
-
                 mApplicationItemList.add(item);
             }
         }
     }
 
     private void updateNetworkState() {
-        isWifiEnabled = isConnectedWifi();
-        isMobilEnabled = isConnectedMobile();
+        isMobile = Networks.isConnectedMobile(mContext);
     }
 
     public List<ApplicationItem> getList() {
         return mApplicationItemList;
-    }
-
-    public boolean isConnectedWifi(){
-        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo info = cm.getActiveNetworkInfo();
-        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
-    }
-
-    public boolean isConnectedMobile(){
-        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo info = cm.getActiveNetworkInfo();
-        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
     }
 
 }
